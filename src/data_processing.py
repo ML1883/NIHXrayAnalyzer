39605ca141d2db7f65aac32a69246d36b0250a2f
ML1883 <mitch.loedeman@proton.me>
2025-03-30 17:28:07 +0200

    Added folder cleaning to test train split, setup dataloader wrapper for batch processing and adjusted various other functions for this. Also some adjustsments in the notebook to account for the changes mentioned above.

--- src/data_processing.py
+++ src/data_processing.py
@@ -59,7 +59,7 @@ def organize_images_by_label_folder(csv_path, image_folder, output_folder, image
             print(f"Diagnosis {subobject}: {num_files} Xray(s)")
 
 
-def sort_images_train_test(source_folder=os.path.join("..", "data", "images_raw", "images"), train_folder=os.path.join("..", "data", "images_train"), test_folder=os.path.join("..", "data", "images_test"), N=10000, train_ratio=0.7):
+def sort_images_train_test(source_folder=os.path.join("..", "data", "images_raw", "images"), train_folder=os.path.join("..", "data", "images_train"), test_folder=os.path.join("..", "data", "images_test"), N=10000, train_ratio=0.7, delete_test_train_folder=False):
     """
     Sorts image files from the source folder into train and test folders.
 
@@ -69,11 +69,31 @@ def sort_images_train_test(source_folder=os.path.join("..", "data", "images_raw"
         test_folder (str): Path to the test folder.
         N (int): Number of images to process, sorted by name in ascending order.
         train_ratio (float): Ratio of images to be placed in the train folder (default is 0.7).
+        delete_test_trainer_folder (bool): Whether to first delete all image files in the test and train folder sorting.
 
     Returns:
         None
     """
     
+    if delete_test_train_folder:
+        for filename in os.listdir(train_folder):
+            if filename.lower().endswith((".jpg", ".jpeg", ".png")):
+                file_path = os.path.join(train_folder, filename)
+                try:
+                    os.remove(file_path)
+                    # print(f"Deleted: {file_path}")
+                except Exception as e:
+                    print(f"Error deleting {file_path}: {e}")
+
+        for filename in os.listdir(test_folder):
+            if filename.lower().endswith((".jpg", ".jpeg", ".png")):
+                file_path = os.path.join(test_folder, filename)
+                try:
+                    os.remove(file_path)
+                    # print(f"Deleted: {file_path}")
+                except Exception as e:
+                    print(f"Error deleting {file_path}: {e}")
+
     # Make sure destinations folders are empty and excist. Otherwise skip
     if os.path.exists(train_folder) and os.path.exists(test_folder) and not os.listdir(train_folder) and not os.listdir(test_folder):
         
